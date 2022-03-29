38a4a7e51afdad9a11764e941fe3a703aa75f2bd
Kousar Kudikangal <skitzmachine@Kousars-MacBook-Air-2.local>
2022-03-29 17:58:21 +0530

    src

---
+++ src/postMainComponent.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const PostMainComponent = () => {
+  return (
+    <View style={{backgroundColor:"#252525",paddingTop:50}}>
+      <Text style={{color:"white"}}>postMainComponent</Text>
+    </View>
+  )
+}
+
+export default PostMainComponent
+
+const styles = StyleSheet.create({})
