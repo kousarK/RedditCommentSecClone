fe6a27016c9c1faca993debd43f62fbce0b7f4f2
Kousar Kudikangal <skitzmachine@Kousars-MacBook-Air-2.local>
2022-03-30 11:30:55 +0530

    safe area adde

--- App.js
+++ App.js
@@ -1,20 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import PostMainComponent from './src/postMainComponent';
-
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import PostMainComponent from "./src/postMainComponent";
+import { NavigationContainer } from '@react-navigation/native';
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import NavigationService from './src/navigation';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <PostMainComponent/>
-    </View>
+    <NavigationContainer ref={navigationRef => NavigationService.setTopLevelNavigator(navigationRef)}>
+    <SafeAreaProvider>
+      <View style={styles.container}>
+        <PostMainComponent />
+      </View>
+    </SafeAreaProvider>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: '#040404',
-    height:"100%"
+    backgroundColor: "#040404",
+    height: "100%",
   },
 });
